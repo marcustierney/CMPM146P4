a4686927b541ed32f1c7469475995199eda90499
Marcus <mstierne@ucsc.edu>
2026-01-30 00:40:19 -0800

    make_operator + declare_operators + simple heuristic added

--- autoHTN.py
+++ autoHTN.py
@@ -31,22 +31,53 @@ def declare_methods(data):
 	pass			
 
 def make_operator(rule):
-	def operator(state, ID):
-		# your code here
-		pass
-	return operator
+    produces = rule.get("Produces", {})
+    consumes = rule.get("Consumes", {})
+    requires = rule.get("Requires", {})
+    time_cost = rule.get("Time", 0)
+
+    def operator(state, ID):
+        if state.time[ID] < time_cost: 
+            return False
+
+        for item, qty in requires.items(): 
+            if getattr(state, item)[ID] < qty:
+                return False
+
+        for item, qty in consumes.items():
+            if getattr(state, item)[ID] < qty:
+                return False
+
+        for item, qty in consumes.items():
+            getattr(state, item)[ID] -= qty
+
+        for item, qty in produces.items():
+            getattr(state, item)[ID] += qty
+
+        state.time[ID] -= time_cost
+
+        return state
+
+    return operator
+
 
 def declare_operators(data):
-	# your code here
-	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
-	pass
+    recipes = data["Recipes"]
+    operators = []
+    for name in recipes:
+        rule = recipes[name]
+        op = make_operator(rule)
+        op.__name__ = "op_" + name.replace(" ", "_")
+        operators.append(op)
+    pyhop.declare_operators(*operators)
 
 def add_heuristic(data, ID):
 	# prune search branch if heuristic() returns True
 	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
 	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
 	def heuristic(state, curr_task, tasks, plan, depth, calling_stack):
-		# your code here
+		if calling_stack.count(curr_task) > 2: #dont allow more than 2 of the same task in the call stack
+			return True
 		return False # if True, prune this branch
 
 	pyhop.add_check(heuristic)
@@ -104,4 +135,5 @@ if __name__ == '__main__':
 	# Hint: verbose output can take a long time even if the solution is correct; 
 	# try verbose=1 if it is taking too long
 	pyhop.pyhop(state, goals, verbose=1)
-	# pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
+	#pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
+	#pyhop.print_operators()
--- manualHTN.py
+++ manualHTN.py
@@ -129,5 +129,5 @@ state.bench = {'agent': 0}
 # pyhop.print_operators()
 # pyhop.print_methods()
 
-# pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 1)], verbose=3)
+pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 1)], verbose=3)
 pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 12)], verbose=3)
